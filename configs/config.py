cad5e001d798880c327525c40a17cc0b54c51e24
gmanasaryan <garri.manasaryan@bostongene.com>
2022-09-30 04:05:53 +0400

    BT-20 remove jsons for now

--- configs/config.py
+++ configs/config.py
@@ -1,11 +1,9 @@
 import json
 from pathlib import Path
+from configs.test_conf import *
 
 # You can specify your token here or create a creds_and_tokens.json in home directory
 with open (Path.home().joinpath('creds_and_tokens.json')) as f:
     credentials = json.load(f)
-
-with open('test.json') as f:
-    test_json = json.load(f)
 
 TOKEN = credentials.get('telegram_token')
