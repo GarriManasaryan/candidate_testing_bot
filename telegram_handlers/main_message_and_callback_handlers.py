29ab1697b0a7e2139c78bbca083f07fc6032274e
gmanasaryan <garri.manasaryan@bostongene.com>
2022-10-08 02:19:49 +0400

    BT-54 File reciever and downloader

--- telegram_handlers/main_message_and_callback_handlers.py
+++ telegram_handlers/main_message_and_callback_handlers.py
@@ -28,9 +28,12 @@ def welcome(message):
         elif text == 't':
             # gsr.docs_downloader_from_drive(fileId = 'https://docs.google.com/document/d/1XwkUaq0hvAjWlzzl2D5t-tgQKAUEHYw7_q3A9vSuG6Y/edit'.split('/')[-2])
             # bot.send_document(chat_id = chat_id, document = open(os.path.join(path_to_download, 'Task.docx'), 'rb'), caption = 'caption', parse_mode='html')
-            bot.send_message(chat_id, full_task_instr, parse_mode='html')
-            bot.send_message(chat_id, just_clinical_task_instr, parse_mode='html')
-            bot.send_message(chat_id, just_excel_instr, parse_mode='html')
+            # bot.send_message(chat_id, just_clinical_task_instr, parse_mode='html')
+            # bot.send_message(chat_id, just_excel_instr, parse_mode='html')
+
+            markup = InlineKeyboardMarkup(row_width=2)
+            markup.add(InlineKeyboardButton('Start', callback_data='start_the_test'))
+            bot.send_message(chat_id, full_task_instr, parse_mode='html', reply_markup=markup)
 
         elif text == '/start':
             clicked = spam_counter.get(chat_id, 0)
@@ -64,9 +67,76 @@ def process_token_from_candidate(message):
     else:
         df_candidate = df_all_candidates[df_all_candidates['token'] == token]
         candidate_info_dict = df_candidate.to_dict('records')[0]
+        token_end = token.split('-')[-1]
+
+        with open(os.path.join(os.getcwd(), 'temps', f'user_{token_end}.json'), 'w') as f:
+            json.dump(candidate_info_dict, f)
+
         bot.send_message(chat_id, file_download)
+
         try:
-            gsr.docs_downloader_from_drive(fileId = candidate_info_dict.get('Link_to_docx_task').split('/')[-2])
-            bot.send_document(chat_id = chat_id, document = open(os.path.join(path_to_download, 'Task.docx'), 'rb'), caption = 'caption', parse_mode='html')
+            fileId = candidate_info_dict.get('Link_to_docx_task').split('/')[-2]
+            gsr.docs_downloader_from_drive(fileId = fileId)
+
+            markup = InlineKeyboardMarkup(row_width=2)
+            markup.add(InlineKeyboardButton('Start', callback_data=f'start_the_test_{token_end}'))
+
+            if candidate_info_dict.get('Send_excel_test_too') == 'Yes':
+                bot.send_message(chat_id, full_task_instr, parse_mode='html', reply_markup=markup)
+
+            else:
+                bot.send_message(chat_id, just_clinical_task_instr, parse_mode='html', reply_markup=markup)
+
+
         except:
             bot.send_message(chat_id, error_downloading_files)
+
+@message_error_handler()
+def get_file_msg(message):
+    file_name = 'Answers_' + message.document.file_name
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open(os.path.join(path_to_download, file_name), 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    bot.send_message(message.chat.id, "Done?")
+
+@bot.callback_query_handler(func=lambda call: True)
+@message_error_handler()
+def callback_handler(call):
+    if call.message:
+        chat_id = call.message.chat.id
+        token_end = str(call.data).split('_')[-1]
+
+        with open(os.path.join(os.getcwd(), 'temps', f'user_{token_end}.json')) as f:
+            candidate_info_dict = json.load(f)
+
+        excel_test_too_flag = candidate_info_dict.get('Send_excel_test_too')
+
+        if 'start_the_test' in str(call.data):
+            if excel_test_too_flag == 'Yes':
+                bot.send_document(chat_id = chat_id, document = open(os.path.join(path_to_download, 'Excel_task.xlsx'), 'rb'), caption = instruction_time_limit_excel, parse_mode='html')
+
+            bot.send_document(chat_id = chat_id, document = open(os.path.join(path_to_download, 'Task.docx'), 'rb'), caption = instruction_time_limit_clinical, parse_mode='html')
+
+            markup = InlineKeyboardMarkup(row_width=2)
+            markup.add(InlineKeyboardButton('Submit answers', callback_data=f'submit_answers_main_{token_end}'))
+            bot.send_message(chat_id, submit_answers, parse_mode='html', reply_markup=markup)
+
+        elif 'submit_answers' in str(call.data):
+
+            if 'main' in str(call.data):
+
+                markup = InlineKeyboardMarkup(row_width=2)
+
+                if excel_test_too_flag == 'Yes':
+                    markup.add(InlineKeyboardButton('Excel task', callback_data=f'submit_answers_dwnl_{token_end}'))
+
+                markup.add(InlineKeyboardButton('Clinical part', callback_data=f'submit_answers_dwnl_{token_end}'))
+
+                bot.send_message(chat_id, choose_answer_section, parse_mode='html', reply_markup=markup)
+
+            elif 'dwnl' in str(call.data):
+                msg = bot.send_message(chat_id, great_send_file)
+                bot.register_next_step_handler(msg, get_file_msg)
