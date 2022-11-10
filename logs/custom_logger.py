0beb402eb88d50c5fb64a3738e587afffe4dc46b
gmanasaryan <garri.manasaryan@bostongene.com>
2022-11-11 03:19:45 +0400

    BT-59 Logging to txt

--- logs/custom_logger.py
+++ logs/custom_logger.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from bot_base.bot_main import *
-import traceback
+import os
 
 def logger(message_or_call, error_msg = 'Successful'):
     date = datetime.strftime(datetime.now() + timedelta(hours=8),'%d-%m-%Y:%H-%M')
@@ -16,14 +16,9 @@ def logger(message_or_call, error_msg = 'Successful'):
 
     status = 'OK' if error_msg == 'Successful' else 'ERROR'
 
-    print(f'{status}| {date} | first_name: {first_name} | username: {username} | user_id: {user_id} | execution: {error_msg} | msg: {msg_text}')
+    final_log = f'{status}| {date} | first_name: {first_name} | user_name: {username} | user_id: {user_id} | execution: {error_msg} | msg: {msg_text}'
 
-# def traceback_log(message_or_call, module):
-#
-#     try:
-#         user_chat_id = message_or_call.from_user.id
-#     except:
-#         user_chat_id = message_or_call.chat.id
-#
-#     with open(os.path.join(logs_folder, f'BOB_errors_{user_chat_id}_{module}.txt'), 'w') as f:
-#         f.write(traceback.format_exc())
+    print(final_log)
+
+    with open(os.path.join(os.getcwd(), 'temp_files', 'bot_logs.txt'), 'a') as f:
+        f.write(f'{final_log}\n')
